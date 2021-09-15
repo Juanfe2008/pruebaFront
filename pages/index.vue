41a33dbc8c22d9a49b34402da0b74294d5612606
Juan <wardkjuan44@gmail.com>
2021-09-15 13:49:33 -0500

    feat: estilos a los mensajes de error

--- pages/index.vue
+++ pages/index.vue
@@ -5,37 +5,38 @@
       <br />
       <div class="form-group row col-sm-10">
         <label class="col-sm-1"> Name </label>
-        <input
-          class="col-sm-5 form-control"
-          placeholder="Name"
-          v-model="name"
-        />
-        <div v-if="submited && !$v.name.required">
-          el campo name es obligatorio
+        <input class="col-sm-5 form-control" placeholder="Name" v-model="name"/>
+
+      </div>
+      <div class="row form-group messaje">
+      <div v-if="submited && !$v.name.required">
+          *The name field is required
         </div>
         <div v-if="submited && !$v.name.minLength">
-          debe tener minimo 5 caracteres
+          *Must have a minimum of 5 characters
         </div>
         <div v-if="submited && !$v.name.maxLength">
-          debe tener maximo 10 caracteres
+          *Must have a maximum of 10 characters
         </div>
       </div>
+
       <div class="form-group row col-sm-10">
         <label class="col-sm-1"> Last Name </label>
-        <input
-          class="form-control col-sm-5"
-          placeholder="Last Name"
-          v-model="lastName"
-        />
+        <input class="form-control col-sm-5" placeholder="Last Name" v-model="lastName"/>
+      </div>
+      <div class="row form-group messaje">
         <div v-if="submited && !$v.lastName.required">
-          el campo Last Name es obligatorio
+          *The last name field is required
         </div>
         <div v-if="submited && !$v.lastName.minLength">
-          debe tener minimo 5 caracteres
+          *Must have a minimum of 5 characters
         </div>
         <div v-if="submited && !$v.lastName.maxLength">
-          debe tener maximo 10 caracteres
+          *Must have a maximum of 10 characters
         </div>
+
+
+
       </div>
       <button type="submit" class="btn btn-success btn-sm" @click="add">
         Agregar
@@ -73,6 +74,9 @@ export default {
       submited: false,
       name: "",
       lastName: "",
+      telephone: '',
+      address: '',
+      email: '',
       user: [],
     };
   },
@@ -111,4 +115,8 @@ export default {
 };
 </script>
 <style>
+  .messaje{
+    padding-left: 6rem !important;
+    color: dimgray;
+  }
 </style>
