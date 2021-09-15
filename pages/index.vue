ee314e6f4c5804523d987e1c6e31b58a0b73fa43
Juan <wardkjuan44@gmail.com>
2021-09-15 11:02:43 -0500

    refactor: arreglo de mensaje de validación

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,15 @@
           placeholder="Name"
           v-model="name"
         />
-        <div v-if="submited && !$v.name.required">el campo name es obligatorio</div>
+        <div v-if="submited && !$v.name.required">
+          el campo name es obligatorio
+        </div>
+        <div v-if="submited && !$v.name.minLength">
+          debe tener minimo 5 caracteres
+        </div>
+        <div v-if="submited && !$v.name.maxLength">
+          debe tener maximo 10 caracteres
+        </div>
       </div>
       <div class="form-group row col-sm-10">
         <label class="col-sm-1"> Last Name </label>
@@ -19,16 +27,25 @@
           placeholder="Last Name"
           v-model="lastName"
         />
-      </div>
-      <button type="submit" class="btn btn-success btn-sm" @click="add">Agregar</button>
-
-      <div v-for="(item) of user" :key="item.name">
-      <div class="alert alert-primary mt-4" role="alert">
-        <div>
-          {{item.name}} {{item.lastName}}
+        <div v-if="submited && !$v.lastName.required">
+          el campo Last Name es obligatorio
+        </div>
+        <div v-if="submited && !$v.lastName.minLength">
+          debe tener minimo 5 caracteres
+        </div>
+        <div v-if="submited && !$v.lastName.maxLength">
+          debe tener maximo 10 caracteres
         </div>
       </div>
-    </div>
+      <button type="submit" class="btn btn-success btn-sm" @click="add">
+        Agregar
+      </button>
+
+      <div v-for="item of user" :key="item.name">
+        <div class="alert alert-primary mt-4" role="alert">
+          <div>{{ item.name }} {{ item.lastName }}</div>
+        </div>
+      </div>
     </form>
   </div>
 </template>
@@ -60,30 +77,30 @@ export default {
     };
   },
   methods: {
-    add: function () {
+    add: function () {},
+    procesar() {
+      this.submited = true;
+      if (this.$v.$invalid) {
+        return false;
+      }
       try {
         this.user.push({
           name: this.name,
           lastName: this.lastName,
         }),
           console.log(this.user);
+          this.submited = false;
         (this.name = ""), (this.lastName = "");
       } catch (error) {
         console.log(error);
       }
-    },
-    procesar() {
-      this.submited = true
-      if (this.$v.$invalid) {
-        return false;
-      }
-      alert(this.name);
     },
   },
   validations: {
     name: {
       required,
       minLength: minLength(5),
+      maxLength: maxLength(10),
     },
     lastName: {
       required,
